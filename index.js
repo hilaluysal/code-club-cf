37d2116bd524c3a7f4fc306a0259238f75376658
hilaluysal <uysalhilaltugba@gmail.com>
2022-11-14 15:19:41 +0000

    if else - slide no 17

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 export default {
-	fetch() {
-		console.log(request.url)
+	fetch(request) {
+		if(request.url == "https://www.cloudflareworkers.com/test")
 		return new Response('Hello worker! I am Hilal!', {
 			headers: {
 				'content-type': 'text/plain',
 			},
 		});
+		else{
+			return new Response ('Error Worker!', {
+				headers: {
+					'content-type' : 'text/plain',
+				}
+			})
+		}	
 	},
 };
