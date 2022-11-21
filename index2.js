704c9083d6b08d989d2afe985ce7b781e761252f
hilaluysal <uysalhilaltugba@gmail.com>
2022-11-21 12:33:20 +0000

    index2.js added

---
+++ index2.js
@@ -0,0 +1,18 @@
+export default {
+    fetch(request) {
+        if(request.method == "POST") {
+        return new Response('You ROCK!', {
+            headers: {
+                'content-type': 'application/json',
+            },
+        });
+    }
+    else{
+            return new Response ('Oops, the request method is not POST!', {
+                headers: {
+                    'content-type' : 'text/plain',
+                },
+            });
+        }   
+    },
+};
