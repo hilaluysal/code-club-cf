e97a51405be7c79010dae5f75511892f76b385f6
hilaluysal <uysalhilaltugba@gmail.com>
2022-11-22 10:55:40 +0000

    test

--- index.js
+++ index.js
@@ -2,18 +2,30 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max)
 }
 
-
 const websiteTable = { 0 : "https://hilaltugbauysal.com",
 				1 : "https://sunnylx.eu",
                 2 : "https://guilherme-menezes.com",
-                3 : "lusostreams.com"}
+                3 : "https://lusostreams.com"}
 
-export default {
+addEventListener('fetch', event => {
+  event.respondWith(redirect(event.request));
+});
+
+async function handleRequest(request) {
+
+	let url = websiteTable[getRandomInt(4)]
+	console.log('Redirected url is:', url)
+	const response = await fetch(request)
+}
+
+/*export default {
     async fetch(request){
-        let number = websiteTable[getRandomInt(4)]
-        let url = websiteTable[number]
+        let url = websiteTable[getRandomInt(4)]
+        console.log(url)
 
-        return request.url
+		return url
+
     }
 
-}
+}*/
+
