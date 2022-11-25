cc29d9e78765479c572c707494500d39d231df7e
hilaluysal <uysalhilaltugba@gmail.com>
2022-11-25 12:51:31 +0000

    2nd assignment

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max)
-}
+export default {
+	fetch(request) {
 
-const websiteTable = { 0 : "https://hilaltugbauysal.com",
-				1 : "https://sunnylx.eu",
-                2 : "https://guilherme-menezes.com",
-                3 : "https://lusostreams.com"}
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * max)
+        }
+        
+        const websiteTable = { 0 : "https://hilaltugbauysal.com",
+                        1 : "https://sunnylx.eu",
+                        2 : "https://guilherme-menezes.com",
+                        3 : "https://lusostreams.com"
+                    }
 
-addEventListener('fetch', event => {
-  event.respondWith(redirect(event.request));
-});
+        
+        async function handleRequest(request) {
 
-async function handleRequest(request) {
+            console.log("Heyy I am inside the function!")
+            let key = getRandomInt(4);
+            let url = websiteTable[key]
+            console.log(key)
+            
+            console.log('Redirected url is:', url)
+           /* return new Response(url, 
+                {
+                    headers:
+                    {
+                        'content-type': 'text/plain',
+                    },
+                });*/
 
-	let url = websiteTable[getRandomInt(4)]
-	console.log('Redirected url is:', url)
-	const response = await fetch(request)
-}
+            return Response.redirect(url, 301)
+        }
 
-/*export default {
-    async fetch(request){
-        let url = websiteTable[getRandomInt(4)]
-        console.log(url)
+        //request.headers.set('Cache-Control', 'no-cache')
+        
+        return handleRequest(request);
 
-		return url
 
-    }
+	},
+            
 
-}*/
+};
 
